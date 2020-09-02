b02dc15c4a6436b34a399a6809cc71dc97228c99
chenshichao <chenshichao@pudutech.com>
2020-09-02 10:34:48 +0800

    新增kulachat-common模块

---
+++ kulachat-common/src/main/java/com/freddy/kulachat/utils/StringUtil.java
@@ -0,0 +1,15 @@
+package com.freddy.kulachat.utils;
+
+/**
+ * @author FreddyChen
+ * @name
+ * @date 2020/09/02 10:30
+ * @email chenshichao@outlook.com
+ * @github https://github.com/FreddyChen
+ */
+public class StringUtil {
+
+    public static void test() {
+
+    }
+}
